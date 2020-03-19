9dc25b629d61d9071c462f3c0401e1b57ac813ae
matate-cyber <62375131+matate-cyber@users.noreply.github.com>
2020-03-19 18:24:55 +0200

    Got a working server-client which is basic but works

--- client_side_communication/socket_io_comm_server.py
+++ client_side_communication/socket_io_comm_server.py
@@ -1,6 +1,41 @@
+import eventlet
 import socketio
 
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+
 class SocketIoCommServer:
-    def __init__(self, port):
-        self._sio = socketio.AsyncServer()
-        self._app = socketio.ASGIApp(self._sio)
+    def __init__(self):
+        eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+        print('upupu')
+        self._clients = []
+
+    @sio.event
+    def connect(self, env):
+        print('connection established to server')
+        print(env)
+        # send all info: force locations, active events
+
+    #
+    # @sio.on('close_event')
+    # def close_event(self, event_id):
+    #     pass
+    #
+    # @sio.on('new_event')
+    # def new_event(self, event_id):
+    #     pass
+
+    '''
+    pushes:
+    - New event to HAMAL. Who takes care.
+    - Hotspots
+    - In connect i send to HAMAL all data(forces, events ACTIVE)
+    '''
+
+
+if __name__ == '__main__':
+    s = SocketIoCommServer()
+    while True:
+        x = 0
+        print('gal')
