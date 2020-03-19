c3022e6d5a25f7de1bd10fd7cd85c5b0f434163e
matate-cyber <62375131+matate-cyber@users.noreply.github.com>
2020-03-19 13:19:10 +0200

    -Fixed event POJO -Added force POJO -Added class for hostpot recognition

--- pojos/event.py
+++ pojos/event.py
@@ -1,12 +1,12 @@
 
 class Event:
     def __init__(self, event_id, timestamp, name, latitude, longitude, type_id, num_participants, event_description):
-        self._event_id = event_id
-        self._timestamp = timestamp
-        self._name = name
-        self._latitude = latitude
-        self._longitude = longitude
-        self._type_id = type_id
-        self._num_participants = num_participants
-        self._event_description = event_description
+        self.event_id = event_id
+        self.timestamp = timestamp
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.type_id = type_id
+        self.num_participants = num_participants
+        self.event_description = event_description
 
---
+++ pojos/force.py
@@ -0,0 +1,11 @@
+
+class Force:
+    def __init__(self, force_id, name, latitude, longitude, type_id, force_description, event_id):
+        self.force_id = event_id
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.type_id = type_id
+        self.force_description = force_description
+        self.event_id = event_id
+
